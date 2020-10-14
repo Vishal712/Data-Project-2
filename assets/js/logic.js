1994e5b2907ea8b91c6413cb628751b5380c6215
Thorne <SHT7249@yum.com>
2020-10-14 08:04:37 -0700

    mapbox

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,4 +1,4 @@
-/ Creating our initial map object
+// Creating our initial map object
 // We set the longitude, latitude, and the starting zoom level
 // This gets inserted into the div with an id of 'map'
 var myMap = L.map("map", {
