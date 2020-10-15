90fef20c67df4b94c3614b52ca26d995bf7e382c
Vishal Patel <vishalpatel@Vishals-MacBook-Pro.local>
2020-10-15 13:10:40 -0700

    Gets data from mongo

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
 from flask import Flask, render_template
 import pymongo
+import pandas as pd
+import get_data
 
 app = Flask(__name__)
 
-# setup mongo connection
-#conn = "mongodb://localhost:27017"
-#client = pymongo.MongoClient(conn)
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
 
-# connect to mongo db and collection
-#db = client.store_inventory
-#produce = db.produce
+# Declare the database
+db = client.NBA_NCAA
+
+# Get the collection from mongo
+NBA = db.NBA
+NCAA = db.NCAA
+NBA_Location = db.NBA_Location
+
+
+#get all entries in the collection and add to a list
+NBA_list = pd.DataFrame(list(NBA.find()))
+NCAA_list = pd.DataFrame(list(NCAA.find()))
+NBA_location_list = pd.DataFrame(list(NBA_Location.find()))
+
+#Convert the lists of dictionaries to pandas df
+NBA_df_mongo = pd.DataFrame(NBA_list)
+NCAA_df_mongo = pd.DataFrame(NCAA_list)
+NBA_location_df_mongo = pd.DataFrame(NBA_location_list)
+
+NBA_df_mongo.to_csv('static/assets/data/NBAData.csv', index = False)
+NCAA_df_mongo.to_csv('static/assets/data/NCAAData.csv', index = False)
+NBA_location_df_mongo.to_csv('static/assets/data/NBALocation.csv', index = False)
 
 
 @app.route("/")
