4a21820e6dccdc582fa563bd0d8356fa401ab7c4
Thorne <SHT7249@yum.com>
2020-10-15 00:06:56 -0700

    mapbox format

---
+++ static/assets/js/mappy.js
@@ -0,0 +1,28 @@
+// Creating map object
+var myMap = L.map("map", {
+  center: [40.7, -73.95],
+  zoom: 11
+});
+
+// Adding a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+d3.csv("assets/data/College_Location.csv", function(data){
+  var markers = L.markerClusterGroup();
+  for (i = 0; i < data.length; i++) {
+    var location = [data[i].lat, data[i].lng]
+    console.log(data)
+    if (location) {
+      markers.addLayer(L.marker([location[0], location[1]])
+        .bindPopup(data[i].player));
+    }
+  }
+myMap.addLayer(markers)
+})
