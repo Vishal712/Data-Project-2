8952574bfda74f79b4dbc8b9193b6eb62f127431
Thorne <SHT7249@yum.com>
2020-10-16 17:15:03 -0700

    mapbox 20

--- static/assets/js/mappy.js
+++ static/assets/js/mappy.js
@@ -1,131 +1,64 @@
-var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-
-// color for legend
-function getColor(d) {
-    return d > 5 ? '#ca4e3f':
-           d > 4 ? '#dbb18a':
-           d > 3 ? '#74bbd3':
-           d > 2 ? '#337ab7':
-           d > 1 ? '#827ab6':
-                   '#242a46';
-}
-
-d3.json(baseURL, function(EQdata) {  
-    createFeatures(EQdata.features);  
+// Creating map object
+var myMap = L.map("map", {
+  center: [38.5, -97.6],
+  zoomSnap: 0.25,
+  zoom: 5
 });
 
-function createFeatures(earthquakeData) {
-  
-  // Give each feature a popup describing the place and time of the earthquake
-  function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>Place: " + feature.properties.place + "</h3><hr><p>Time: " + new Date(feature.properties.time) + "</p><p>Magnitude: " + feature.properties.mag + "</p>");    
-  }
-
-  // // pull icon
-  // var ballIcon = L.icon({
-  //   iconUrl: '../static/images/ballicon.png',
-  //   iconSize: [30,30],
-  //   popupAnchor: [-10, -30],
-  // }); 
-
-  // Run the onEachFeature function once for each piece of data in the array
-  var earthquakes = L.geoJson(earthquakeData, {
-    pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, {radius: feature.properties.mag * 5,
-                                      fillColor: getColor(feature.properties.mag),
-                                      color: "#000",
-                                      weight: 1,
-                                      opacity: 1,
-                                      fillOpacity: 0.8});
-    },
-    onEachFeature: onEachFeature
-  });
-
-  // Sending our earthquakes layer to the createMap function
-  createMap(earthquakes);
-}
-
-function createMap(earthquakes) {
-var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/hyrum78/ckg9tsvf803p519pet1f9cbmk/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaHlydW03OCIsImEiOiJja2c1bGdpZjQwdWIwMnJtc2dscmd0eHFzIn0.lEHm3LRxyN6_azwPjzIf5A", {
+// Adding tile layer to the map
+L.tileLayer("https://api.mapbox.com/styles/v1/hyrum78/ckg9tsvf803p519pet1f9cbmk/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaHlydW03OCIsImEiOiJja2c1bGdpZjQwdWIwMnJtc2dscmd0eHFzIn0.lEHm3LRxyN6_azwPjzIf5A", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
   id: "mapbox/streets-v11",
   accessToken: API_KEY
-  });
-
-  // Define a baseMaps object to hold our base layers
-  var baseMaps = {
-      "Street Map": streetmap
-  };
-
-  // Create overlay object to hold our overlay layer
-  var overlayMaps = {
-      Earthquakes: earthquakes
-  };
-
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
-  var myMap = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 5,
-    layers: [streetmap, earthquakes]
-  });
-
-  // Add the layer control to the map
-  L.control.layers(baseMaps, overlayMaps, {
-      collapsed: true
   }).addTo(myMap);
 
-  // Set up legend
-  var legend = L.control({
-      position: "bottomright"
-  });
+  // pull icon
+  var ballIcon = L.icon({
+    iconUrl: '../static/images/ballicon.png',
+    iconSize: [30,30],
+    popupAnchor: [-10, -30],
+  }); 
 
-  legend.onAdd = function () {
+d3.csv("../static/assets/data/NBALocation.csv", function(data) {
 
-  var div = L.DomUtil.create('div', 'info legend'),
-      magnitudes = [0, 1, 2, 3, 4, 5];
+  // Create a new marker cluster group
+  var markers = L.markerClusterGroup(
+  //   {
+  //   iconCreateFunction: function(cluster) {
+  //     return L.divIcon({ html: '<b>' + cluster.getChildCount() + '</b>' });
+  //   }
+  // }
+  );
 
-  for (var i = 0; i < magnitudes.length; i++) {
-      div.innerHTML +=
-          '<i style="background:' + getColor(magnitudes[i] + 1) + '"></i> ' + 
-  + magnitudes[i] + (magnitudes[i + 1] ? ' - ' + magnitudes[i + 1] + '<br>' : ' + ');
+  //Disable all of the defaults:
+  // spiderfyOnMaxZoom: false, showCoverageOnHover: false, zoomToBoundsOnClick: false
+
+  // Loop through data
+  for (var i = 0; i < data.length; i++) {
+
+    // Set the data location property to a variable
+    var location = [data[i].lat, data[i].lng];
+    console.log(data)
+
+    // Check for location property
+    if (location) {
+
+      // Add a new marker to the cluster group and bind a pop-up
+      markers.addLayer(L.marker([location[0], location[1]],{icon: ballIcon})
+        .bindPopup("School: " + data[i].school + "<br>" +
+                    "Avg NBA Player Stats:" + "<br>" + 
+                    "PPG " + data[i].PPG + "<br>" + 
+                    "APG " + data[i].APG + "<br>" + 
+                    "RPG " + data[i].RPG));
+    }
+
   }
 
-    return div;
-};
-
-legend.addTo(myMap);
-
-// .addTo(myMap);
-
-// d3.csv("../static/assets/data/NBALocation.csv", function(data) {
-
-//   // Create a new marker cluster group
-//   var markers = L.markerClusterGroup();
-
-//   // Loop through data
-//   for (var i = 0; i < data.length; i++) {
-
-//     // Set the data location property to a variable
-//     var location = [data[i].lat, data[i].lng];
-//     console.log(data)
-
-//     // Check for location property
-//     if (location) {
-
-//       // Add a new marker to the cluster group and bind a pop-up
-//       markers.addLayer(L.marker([location[0], location[1]])
-//         .bindPopup(data[i].Name));
-//     }
-
-//   }
-
-//   // Add our marker cluster layer to the map
-//   myMap.addLayer(markers);
+  // Add our marker cluster layer to the map
+  myMap.addLayer(markers);
   
-
-}
+ 
+});
